15860b870904a99884b2e8fce7b53b20983127e5
Jordana Loreto <jordanaloreto45@gmail.com>
2024-08-14 16:08:34 -0300

    ajustes

--- components/template.php
+++ components/template.php
@@ -1,52 +1,81 @@
 
 <body>
-    <!-- Barra Superior -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Sistema</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Livros</a>
-                    </li>
+<div class="container mt-4">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <div class="dropdown">
+                <a class="navbar-brand dropdown-toggle d-flex align-items-center" href="#" id="dropdownUserLink" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="rounded-circle border border-dark p-2 me-2"></span>
+                    <span class="user-name">Jordana</span>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownUserLink">
+                    <li><a class="dropdown-item" href="#">Configurações</a></li>
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
                 </ul>
-                <span class="navbar-text">
-                    Usuário Logado: Jordana
-                </span>
+            </div>
+            <div>
+                <div class="dropdown">
+                    <a class="navbar-brand dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        Livros
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Livros</a></li>
+                        <li><a class="dropdown-item" href="#">Filmes</a></li>
+                        <li><a class="dropdown-item" href="#">Séries</a></li>
+                        <li><a class="dropdown-item" href="#">Geral</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
 
-    <div class="container-fluid">
-        <div class="row">
+    <div class="row">
+        <div class="col-md-3">
             <!-- Sidebar -->
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                <div class="position-sticky pt-3">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
-                                Cadastrar
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Listas
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <!-- Conteúdo principal -->
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <h1 class="h2">Conteúdo Principal</h1>
-                <!-- Conteúdo da página vai aqui -->
-            </main>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastrar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Listas</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-9">
+            <!-- Conteúdo Principal -->
+            <h1>Conteúdo Principal</h1>
+            <!-- O restante do conteúdo -->
         </div>
     </div>
+</div>
+
+<style>
+    .navbar {
+        padding: 10px;
+        border: 2px solid black;
+        border-radius: 25px;
+    }
+
+    .user-name {
+        font-weight: bold;
+    }
+
+    .rounded-circle {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        height: 40px;
+        width: 40px;
+    }
+
+    .navbar-brand {
+        font-weight: bold;
+        margin-right: 0;
+    }
+</style>
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
