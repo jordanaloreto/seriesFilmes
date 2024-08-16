16fdd1cc5e8ce3ae59b636d086148d8a35f386aa
Jordana Loreto <jordanaloreto45@gmail.com>
2024-08-16 11:00:21 -0300

    ajuste

--- components/template.php
+++ components/template.php
@@ -1,4 +1,3 @@
-
 <body>
 <div class="container mt-4">
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
@@ -19,32 +18,37 @@
                         Livros
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">Livros</a></li>
-                        <li><a class="dropdown-item" href="#">Filmes</a></li>
-                        <li><a class="dropdown-item" href="#">Séries</a></li>
-                        <li><a class="dropdown-item" href="#">Geral</a></li>
+                        <li><a class="dropdown-item" href="form_livro.php">Livros</a></li>
+                        <li><a class="dropdown-item" href="form_filme.php">Filmes</a></li>
+                        <li><a class="dropdown-item" href="form_serie.php">Séries</a></li>
+                        <li><a class="dropdown-item" href="form_geral.php">Geral</a></li>
                     </ul>
                 </div>
             </div>
         </div>
     </nav>
 
-    <div class="row">
-        <div class="col-md-3">
-            <!-- Sidebar -->
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Cadastrar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Listas</a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-md-9">
-            <!-- Conteúdo Principal -->
-            <h1>Conteúdo Principal</h1>
-            <!-- O restante do conteúdo -->
+    <!-- Container with rounded corners for sidebar and content -->
+    <div class="container mt-4 p-4" style="border: 2px solid black; border-radius: 25px;">
+        <div class="row">
+            <div class="col-md-3" style="border-right: 2px solid black;">
+                <!-- Sidebar -->
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="#">Cadastrar</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="#">Listas</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-9" style="border-left: 2px solid black;">
+                <!-- Linha amarela horizontal -->
+                <div style="border-bottom: 2px solid black; margin-bottom: 10px;"></div>
+                <!-- Conteúdo Principal -->
+                <h1>Conteúdo Principal</h1>
+                <!-- O restante do conteúdo -->
+            </div>
         </div>
     </div>
 </div>
@@ -72,11 +76,12 @@
         font-weight: bold;
         margin-right: 0;
     }
+
+    .list-group-item a {
+        text-decoration: none;
+    }
 </style>
 
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
